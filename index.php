f4666b699d45d70e5dc25d4f9171be04a52debc3
morelia <moreli.macne@gmail.com>
2023-04-14 16:20:58 -0400

    Tareas 5to Dayli Meeting

--- index.php
+++ index.php
@@ -18,15 +18,15 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body >
+<body class="bg-primary bg-opacity-50">
 
-    <section class="d-flex flex-column justify-content-center">
+    <section class="d-flex flex-column min-vh-100 justify-content-center aling-items-center">
       <div class="container">
         <div class="row">
-            <div class="rounded shadow bg-white">
+            <div class="col-md-10 mx-auto rounded shadow bg-white">
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="m-5">
+                        <div class="m-5 text-center">
                             <h1>Bienbenido!</h1>
                         </div>
                         <form class="m-5">
@@ -45,9 +45,14 @@
                                         <label class="form-check-label" for="remember-me">Recordar contraseña</label>
                                     </div>
                                 </div>
+                                <!-- <div class="col-6">
+                                    <div class="text-end">
+                                        <a href="#">Forgot Password?</a>
+                                    </div>
+                                </div>-->
                                 <div>
                                <!-- <button type="submit" class="form-control btn btn-primary mt-3">Enviar</button>-->
-                                    <input type="submit" class="form-control">
+                                    <input type="submit" class="form-control btn btn-primary mt-3">
                                 </div>
                             </div>
                         </form>
